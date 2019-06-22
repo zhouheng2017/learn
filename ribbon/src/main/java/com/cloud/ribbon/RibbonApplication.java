7f5081779be53f490c474f81c407c637f6c7a8b0
zhouheng <zhouheng@weilaihui.com>
2019-06-22 18:53:42 +0800

    Feign优化

--- ribbon/src/main/java/com/cloud/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/cloud/ribbon/RibbonApplication.java
@@ -1,11 +1,14 @@
 package com.cloud.ribbon;
 
+import com.cloud.feign.service.SchedulServiceHi;
+import com.cloud.ribbon.service.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableHystrix
+@EnableFeignClients(basePackageClasses = {SchedulServiceHi.class})
 public class RibbonApplication {
 
     public static void main(String[] args) {
--- ribbon/src/main/java/com/cloud/ribbon/controller/HelloController.java
+++ ribbon/src/main/java/com/cloud/ribbon/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.cloud.ribbon.controller;
 
+import com.cloud.feign.service.SchedulServiceHi;
 import com.cloud.ribbon.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,19 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
+    @Autowired
+    private SchedulServiceHi schedulServiceHi;
+
     @RequestMapping("/hello")
     public String hello(String name){
         return helloService.hiService(name);
     }
 
+    @RequestMapping("/hi")
+    public String hi(String name) {
+
+        String s = schedulServiceHi.sayHiFromClientOne(name);
+        System.out.println(s);
+        return s;
+    }
 }
