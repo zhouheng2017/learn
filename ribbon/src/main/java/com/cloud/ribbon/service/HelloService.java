ab702c7825acc8a1920a929e8a89189098302806
zhouheng <zhouheng@weilaihui.com>
2019-06-22 18:16:11 +0800

    Feign优化

--- ribbon/src/main/java/com/cloud/ribbon/service/HelloService.java
+++ ribbon/src/main/java/com/cloud/ribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.cloud.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,12 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "HiError")
     public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVIE-HELLO/hi?name=" + name, String.class);
+        return restTemplate.getForObject("http://SERVICE-HELLO/hi?name=" + name, String.class);
+    }
+
+    public String HiError(String name) {
+        return "hi, " +name +",sorry,error!";
     }
 }
