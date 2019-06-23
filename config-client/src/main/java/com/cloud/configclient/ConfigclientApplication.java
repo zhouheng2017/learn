cc28ac011a0e9946b55f70aa11c59ce6442508f2
lifeihong178 <47975681+lifeihong178@users.noreply.github.com>
2019-06-23 17:58:56 +0800

    zipkin

--- config-client/src/main/java/com/cloud/configclient/ConfigclientApplication.java
+++ config-client/src/main/java/com/cloud/configclient/ConfigclientApplication.java
@@ -1,11 +1,8 @@
 package com.cloud.configclient;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableEurekaClient
