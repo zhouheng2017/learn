44d494f86faf98ed1f05d91f7584f22546db6a1f
zhouheng <zhouheng@weilaihui.com>
2019-06-23 14:18:02 +0800

    config

--- config-client/src/main/java/com/cloud/configclient/ConfigclientApplication.java
+++ config-client/src/main/java/com/cloud/configclient/ConfigclientApplication.java
@@ -1,14 +1,25 @@
 package com.cloud.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class ConfigclientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigclientApplication.class, args);
     }
 
+
+    @Value("${foo}")
+    String value;
+
+    @RequestMapping("/hi")
+    public String hello() {
+        return value;
+    }
 }
---
+++ config-client/src/main/java/com/cloud/configclient/controller/ConfigController.java
@@ -0,0 +1,26 @@
+package com.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zhouheng
+ * @Created: with IntelliJ IDEA.
+ * @Description:
+ * @Date: 2019-06-23
+ * @Time: 13:59
+ */
+
+@RequestMapping("/")
+public class ConfigController {
+
+    @Value("${foo}")
+    private String value;
+
+    @RequestMapping("/hi")
+    public String hello() {
+        return value;
+    }
+
+}
