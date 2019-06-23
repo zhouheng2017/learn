290de92d0b9d9ed2a55798383a42ef14f73d8936
lifeihong178 <47975681+lifeihong178@users.noreply.github.com>
2019-06-23 16:29:21 +0800

    高可用集群配置中心

--- config-client/src/main/java/com/cloud/configclient/ConfigclientApplication.java
+++ config-client/src/main/java/com/cloud/configclient/ConfigclientApplication.java
@@ -3,10 +3,12 @@ package com.cloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigclientApplication {
 
     public static void main(String[] args) {
