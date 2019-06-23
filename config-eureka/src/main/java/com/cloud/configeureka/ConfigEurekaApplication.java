8bc9ac9cc4534cacd83d45337a31efef5328e76a
zhouheng <zhouheng@weilaihui.com>
2019-06-23 15:26:46 +0800

    'sdf'

---
+++ config-eureka/src/main/java/com/cloud/configeureka/ConfigEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.configeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigEurekaApplication.class, args);
+    }
+
+}
